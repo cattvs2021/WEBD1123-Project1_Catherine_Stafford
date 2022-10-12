3917ef2ce33700eeec8976f558c8ddef28ca3bbb
“cattvs2021” <cattvs2021@gmail.com>
2022-10-12 12:17:19 -0500

    added 4 divs in div wrapper

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
 let addToHead = document.querySelector("head");
 let cssLink = document.createElement("link");
 addToHead.appendChild(cssLink);
+cssLink.setAttribute("rel", "stylesheet");
+cssLink.setAttribute("href", "css/style.css");
+let wrapper = document.createElement("div");
+wrapper.classList.add("wrapper");
+let bodY = document.querySelector("body");
+bodY.appendChild(wrapper);
+let i = 0
+for (let i = 0; i < 4; i++) {
+    let divs = document.createElement("div");
+    wrapper.appendChild(divs);
+    divs.classList.add("wrapperDivs");
+}
