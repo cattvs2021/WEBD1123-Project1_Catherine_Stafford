289fd049d30aeefc654de1b183b14f676fc69353
“cattvs2021” <cattvs2021@gmail.com>
2022-10-12 12:35:42 -0500

    added colorsArray

--- js/app.js
+++ js/app.js
@@ -13,3 +13,9 @@ for (let i = 0; i < 4; i++) {
     wrapper.appendChild(divs);
     divs.classList.add("wrapperDivs");
 }
+var color1 = "linear-gradient(135deg, #0B132B 0%, #3A506B 100%)";
+var color2 = "linear-gradient(330deg, #5BC0BE 0%, #3A506B 100%)";
+var color3 = "linear-gradient(35deg, #0B132B 0%, #5BC0BE 100%)";
+var color4 = "linear-gradient(235deg, #5BC0BE 0%, #3A506B 100%)";
+let colorsArray = [color1,color2,color3,color4];
+console.log(colorsArray);
